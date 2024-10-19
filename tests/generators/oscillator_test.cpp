caf1bb661b5a60282642e5a8cd88be30f0a79558
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-10-19 18:51:32 -0500

    Add another test for oscillator

--- tests/generators/oscillator_test.cpp
+++ tests/generators/oscillator_test.cpp
@@ -14,8 +14,8 @@ TEST(oscillator_tests, generate_test)
     Oscillator osc(context, 440.0f);
 
     EXPECT_FLOAT_EQ(osc.Generate(), 0.0f);
-    EXPECT_FLOAT_EQ(osc.Generate(), 0.062643729);
-    EXPECT_FLOAT_EQ(osc.Generate(), 0.1250467);
+    EXPECT_FLOAT_EQ(osc.Generate(), 0.062643729f);
+    EXPECT_FLOAT_EQ(osc.Generate(), 0.1250467f);
 }
 
 TEST(oscillator_tests, reset_test)
@@ -28,10 +28,29 @@ TEST(oscillator_tests, reset_test)
     Oscillator osc(context, 440.0f);
 
     EXPECT_FLOAT_EQ(osc.Generate(), 0.0f);
-    EXPECT_FLOAT_EQ(osc.Generate(), 0.062643729);
+    EXPECT_FLOAT_EQ(osc.Generate(), 0.062643729f);
 
     osc.Reset();
 
     EXPECT_FLOAT_EQ(osc.Generate(), 0.0f);
-    EXPECT_FLOAT_EQ(osc.Generate(), 0.062643729);
+    EXPECT_FLOAT_EQ(osc.Generate(), 0.062643729f);
+}
+
+TEST(oscillator_tests, set_frequency_test)
+{
+    Context context {
+        44100,
+        2,
+        16
+    };
+    Oscillator osc(context, 440.0f);
+
+    EXPECT_FLOAT_EQ(osc.Generate(), 0.0f);
+    EXPECT_FLOAT_EQ(osc.Generate(), 0.062643729f);
+
+    osc.Reset();
+    osc.SetFrequency(220.0f);
+
+    EXPECT_FLOAT_EQ(osc.Generate(), 0.0f);
+    EXPECT_FLOAT_EQ(osc.Generate(), 0.031337444f);
 }
