8bf6e5f6f0b249a54d7a45f34ae9049a3452f563
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-10-23 20:52:06 -0500

    Add comments

--- src/modulators/adsr.cpp
+++ src/modulators/adsr.cpp
@@ -20,18 +20,18 @@ float AdsrEnvelopeModulator::Modulate()
     switch (m_stage) {
     case AdsrStage::ATTACK:
         value = position / m_envelope.attack;
-        Update(m_envelope.attack, AdsrStage::DECAY);
+        Update(m_envelope.attack, AdsrStage::DECAY, true);
         break;
     case AdsrStage::DECAY:
         value = (((m_envelope.sustain - 1.0f) / m_envelope.decay) * position) + 1.0f;
-        Update(m_envelope.decay, AdsrStage::SUSTAIN);
+        Update(m_envelope.decay, AdsrStage::SUSTAIN, true);
         break;
     case AdsrStage::SUSTAIN:
         value = m_envelope.sustain;
         break;
     case AdsrStage::RELEASE:
         value = ((-m_envelope.sustain / m_envelope.release) * position) + m_envelope.sustain;
-        Update(m_envelope.release, AdsrStage::IDLE);
+        Update(m_envelope.release, AdsrStage::IDLE, true);
         break;
     case AdsrStage::IDLE:
     default:
@@ -62,13 +62,13 @@ void AdsrEnvelopeModulator::Reset()
 void AdsrEnvelopeModulator::SetAttackTime(float attackTimeMs)
 {
     m_envelope.attack = attackTimeMs;
-    CheckEnvelopePosition(m_envelope.attack, AdsrStage::DECAY);
+    Update(m_envelope.attack, AdsrStage::DECAY, false);
 }
 
 void AdsrEnvelopeModulator::SetDecayTime(float decayTimeMs)
 {
     m_envelope.decay = decayTimeMs;
-    CheckEnvelopePosition(m_envelope.decay, AdsrStage::SUSTAIN);
+    Update(m_envelope.decay, AdsrStage::SUSTAIN, false);
 }
 
 void AdsrEnvelopeModulator::SetSustainLevel(float sustainLevel)
@@ -79,17 +79,15 @@ void AdsrEnvelopeModulator::SetSustainLevel(float sustainLevel)
 void AdsrEnvelopeModulator::SetReleaseTime(float releaseTimeMs)
 {
     m_envelope.release = releaseTimeMs;
-    CheckEnvelopePosition(m_envelope.release, AdsrStage::IDLE);
+    Update(m_envelope.release, AdsrStage::IDLE, false);
 }
 
-void AdsrEnvelopeModulator::Update(float stageDuration, AdsrStage nextStage)
+void AdsrEnvelopeModulator::Update(float stageDuration, AdsrStage nextStage, bool incrementSampleCount)
 {
-    m_samplesSinceLastStage++;
-    CheckEnvelopePosition(stageDuration, nextStage);
-}
+    if (incrementSampleCount) {
+        m_samplesSinceLastStage++;
+    }
 
-void AdsrEnvelopeModulator::CheckEnvelopePosition(float stageDuration, AdsrStage nextStage)
-{
     float msPerSample = 1000.0f / (float)m_context.sampleRate;
     if ((float)m_samplesSinceLastStage * msPerSample >= stageDuration) {
         m_samplesSinceLastStage = 0;
--- src/modulators/adsr.h
+++ src/modulators/adsr.h
@@ -1,10 +1,12 @@
 #pragma once
 
-#include <cmath>
-
 #include "../audio/context.h"
 
 namespace cortex {
+/**
+ * The configuration of an ADSR envelope curve including durations
+ * for the attack, decay, and release stages as well as a sustain level.
+ */
 struct AdsrEnvelope {
     float attack;
     float decay;
@@ -12,6 +14,9 @@ struct AdsrEnvelope {
     float release;
 };
 
+/**
+ * The stages of an ADSR envelope, including an "idle" stage when not in use.
+ */
 enum class AdsrStage {
     IDLE,
     ATTACK,
@@ -24,9 +29,24 @@ const AdsrEnvelope DEFAULT_ADSR_ENVELOPE = {
     100.0f, 200.0f, 1.0f, 1000.0f
 };
 
+/**
+ * The ADsrEnvelopeModulator class is a modulation source
+ * that is based off of an ADSR envelope generator.
+ */
 class AdsrEnvelopeModulator {
 public:
+    /**
+     * Creates an ADSR envelope modulator.
+     *
+     * @param context The DSP context to be used by the envelope.
+     * @param envelope The envelope configuration to initialize the class with.
+     * @return AdsrEnvelopeModulator
+     */
     AdsrEnvelopeModulator(Context& context = DEFAULT_CONTEXT, AdsrEnvelope envelope = DEFAULT_ADSR_ENVELOPE);
+
+    /**
+     * Frees any memory allocated by the modulator.
+     */
     ~AdsrEnvelopeModulator();
 
     /**
@@ -34,26 +54,57 @@ public:
      */
     float Modulate();
 
+    /**
+     * Starts the envelope from its attack phase.
+     */
     void Trigger();
+
+    /**
+     * Starts the envelope from its release phase.
+     */
     void Release();
+
+    /**
+     * Re-initializes the modulator, ready to be re-triggered.
+     */
     void Reset();
 
+    /**
+     * Sets the attack time of the envelope.
+     *
+     * @param attackTimeMs The new attack time for the envelope.
+     */
     void SetAttackTime(float attackTimeMs);
+
+    /**
+     * Sets the decay time of the envelope.
+     *
+     * @param decayTimeMs The new decay time for the envelope.
+     */
     void SetDecayTime(float decayTimeMs);
+
+    /**
+     * Sets the sustain level of the envelope.
+     *
+     * @param sustainLevel The new sustain level for the envelope.
+     */
     void SetSustainLevel(float sustainLevel);
+
+    /**
+     * Sets the release time of the envelope.
+     *
+     * @param releaseTimeMs The new release time for the envelope.
+     */
     void SetReleaseTime(float releaseTimeMs);
 
 private:
-    void Update(float stageDuration, AdsrStage nextStage);
-    void CheckEnvelopePosition(float stageDuration, AdsrStage nextStage);
+    // Checks and updates the modulator's state if necessary
+    void Update(float stageDuration, AdsrStage nextStage, bool incrementSampleCount);
 
     Context& m_context;
 
     AdsrEnvelope m_envelope;
     AdsrStage m_stage = AdsrStage::IDLE;
     size_t m_samplesSinceLastStage = 0;
-
-    // The amount to which the modulation is applied.
-    float m_depth;
 };
 }
