b0b28bba617e52e7a13a749a79a0595215aa3d3f
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-12-01 13:42:53 -0600

    Format code

--- src/processors/filters/filter.cpp
+++ src/processors/filters/filter.cpp
@@ -4,7 +4,9 @@
 using namespace neuron;
 
 Filter::Filter(Context& context, float cutoffFrequency)
-    : p_cutoffFrequency(cutoffFrequency), m_context(context), m_previousOutput(0.0f)
+    : p_cutoffFrequency(cutoffFrequency)
+    , m_context(context)
+    , m_previousOutput(0.0f)
 {
     SetCutoffFrequency(cutoffFrequency);
 }
