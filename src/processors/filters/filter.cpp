c200698e30a02723328e4f519b00c95025e5692d
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-12-01 13:11:23 -0600

    Re-order again

--- src/processors/filters/filter.cpp
+++ src/processors/filters/filter.cpp
@@ -4,8 +4,7 @@
 using namespace neuron;
 
 Filter::Filter(Context& context, float cutoffFrequency)
-    : m_context(context)
-    , p_cutoffFrequency(cutoffFrequency), m_previousOutput(0.0f)
+    : p_cutoffFrequency(cutoffFrequency), m_context(context), m_previousOutput(0.0f)
 {
     SetCutoffFrequency(cutoffFrequency);
 }
